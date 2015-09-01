d0dff10062150f97c70b0bd1c1fcb62b2e41fe85
Hoang loc <mcu.stm32@gmail.com>
2015-09-01 13:53:07 +0700

    Create RKC_SystemClockConfig.h

---
+++ inc/RKC_SystemClockConfig.h
@@ -0,0 +1,20 @@
+/*******************************************************************************
+*Rocket Kit - System Clock Library
+*Date: 29/08/2015
+*Created by:hoangloc
+*******************************************************************************/
+#ifndef _RKC_SYSTEMCLOCKCONFIG_H_
+#define _RKC_SYSTEMCLOCKCONFIG_H_
+#include "stm8s.h"
+void RKC_HSICLK_16MHZ(void);
+void RKC_HSICLK_8MHZ(void);
+void RKC_HSICLK_4MHZ(void);
+void RKC_HSICLK_2MHZ(void);
+void RKC_HSICLK_1MHZ(void);
+
+void RKC_HSECLK_16MHZ(void);
+void RKC_HSECLK_8MHZ(void);
+void RKC_HSECLK_4MHZ(void);
+void RKC_HSECLK_2MHZ(void);
+void RKC_HSECLK_1MHZ(void);
+#endif
